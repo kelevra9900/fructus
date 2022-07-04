f407e48c082a134d84b940bd686a75db174f90ff
Roger Torres <torresroger445@gmail.com>
2022-07-04 11:29:51 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Roger Torres on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
